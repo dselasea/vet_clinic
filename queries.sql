5d4f10b94646c4b6d329a5516f82a8abd34ebef9
Daniel <dselasea@gmail.com>
2023-05-02 21:42:06 +0000

    modify query name from Gubamon to Gabumon

--- queries.sql
+++ queries.sql
@@ -26,7 +26,7 @@ WHERE neutered=TRUE;
 
 -- All animals not named Gabumon.
 SELECT * FROM animals
-WHERE name <> 'Gubamon';
+WHERE name <> 'Gabumon';
 
 -- All animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals

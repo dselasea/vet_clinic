ed18ecce8e06d8f25f52be52d3558d8322214ad5
Daniel <dselasea@gmail.com>
2023-05-03 00:09:38 +0000

    add queries to queries.sql

--- queries.sql
+++ queries.sql
@@ -82,3 +82,29 @@ COMMIT;
 
 -- Update all animals' weights that are negative to be their weight multiplied by -1
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, SUM(escape_attempts) AS total_escapes
+FROM animals
+GROUP BY neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) AS avg_escapes
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
+
